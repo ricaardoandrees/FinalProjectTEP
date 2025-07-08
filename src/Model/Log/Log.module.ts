3cb9299e125f08b7f8f39f249a10e3cdd28a6cda
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-07-07 20:35:11 -0400

    fix: fixed Log.service I GUESS

--- src/Model/Log/Log.module.ts
+++ src/Model/Log/Log.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Log } from './Log.entity';
 import { LogController } from './Log.controller'; 
-import { LogService } from './log.service';
+import { LogService } from './Log.service';
 import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
